9ea577c7effa48baf81e37d16ef208410fb7cfd3
phomemyintmo <phomemyintmo012@gmail.com>
2026-07-21 15:08:17 +0630

    changes

--- components/ScrollToTop.tsx
+++ components/ScrollToTop.tsx
@@ -4,37 +4,44 @@ import { ChevronsUp } from "lucide-react";
 import { useEffect, useState } from "react";
 
 export default function ScrollToTop() {
-  const [isVisible, setIsVisible] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsVisible(window.scrollY > 500);
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.innerHeight + window.scrollY;
+            const pageHeight = document.documentElement.scrollHeight;
+
+            const isAtBottom = scrollPosition >= pageHeight - 10;
+
+            setIsVisible(isAtBottom);
+        };
+
+        window.addEventListener("scroll", handleScroll, {
+            passive: true,
+        });
+
+        handleScroll();
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
     };
 
-    window.addEventListener("scroll", handleScroll, {
-      passive: true,
-    });
+    if (!isVisible) return null;
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-
-  if (!isVisible) return null;
-
-  return (
-    <button
-  onClick={scrollToTop}
-  aria-label="Scroll to top"
-  className="group fixed bottom-6 left-6 z-50 rounded-full border border-black bg-background p-3 shadow-md transition-transform hover:-translate-y-1"
->
-<ChevronsUp className="size-5 animate-bounce" /></button>
-  );
+    return (
+        <button
+            onClick={scrollToTop}
+            aria-label="Scroll to top"
+            className="group fixed bottom-6 left-6 z-50 rounded-full p-2 bg-background  shadow-md transition-transform hover:-translate-y-1"
+        >
+            <ChevronsUp className="size-5 animate-bounce" /></button>
+    );
 }
